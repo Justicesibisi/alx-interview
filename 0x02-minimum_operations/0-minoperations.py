d74504ade14ee46633412c4e0273a2235ad68d13
Justice Sibisi <djmangsa@gmail.com>
2024-10-29 19:51:07 +0200

    Test changes

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -23,25 +23,22 @@ Number of operations: 6
 def minOperations(n):
     if n <= 1:
         return 0
-    print("n before loop", n)
 
-    divisor =2
-
+    divisor = 2
     num_of_operations = 0
 
     while n > 1:
         if n % divisor == 0:
-            print("divisor is: ", divisor)
             n = n / divisor
-            print("\nn = n / divisor", n)
-
             num_of_operations += divisor
-            print("\nnum of ops", num_of_operations)
         else:
             divisor += 1
-            print("divisor after increment", divisor)
 
     return num_of_operations
-        
+
+# Test cases
 n = 12
-print(minOperations(n))
+print(minOperations(n))  # Expected output: 7
+
+n = 9
+print(minOperations(n))  # Expected output: 6
