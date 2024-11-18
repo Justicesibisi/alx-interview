6b33a9928a661c1f4434d800ff78741290d4c10a
Justicesibisi <djmangsa@gmail.com>
2024-11-18 20:29:08 +0000

    rotate algo

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,6 +6,7 @@ This module provides a function to rotate a 2D matrix 90 degrees clockwise
 in place.
 """
 
+
 def rotate_2d_matrix(matrix):
     """
     Rotates an n x n 2D matrix 90 degrees clockwise in place.
